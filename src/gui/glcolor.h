d43de88665867d75800655ac98f9998f8bd1d389
Jan <delojoba@telecom-paristech.fr>
2011-05-14 21:38:00 +0200

    solved some problems

--- src/gui/glcolor.h
+++ src/gui/glcolor.h
@@ -23,9 +23,10 @@ public:
         for(int i=0; i<ntheta; i++){
             //phi
             for(int j=0; j<nphi; j++){
-                color[i][j] = vec3Df(i/(float)(ntheta-1),i/(float)(ntheta-1),i/(float)(ntheta-1)); //theta
-//                color[i][j] = vec3Df(j/(float)(nphi-1),j/(float)(nphi-1),j/(float)(nphi-1)); //phi
-//                color[i][j] = vec3Df(1,1,1);                            //blanc
+//                color[i][j] = vec3Df(1,1,1);                                                          //blanc
+//                color[i][j] = vec3Df(i/(float)(ntheta-1),i/(float)(ntheta-1),i/(float)(ntheta-1));    //theta
+//                color[i][j] = vec3Df(j/(float)(nphi-1),j/(float)(nphi-1),j/(float)(nphi-1));          //phi
+                color[i][j] = vec3Df(i/(float)(ntheta-1),j/(float)(nphi-1),0);                        //theta,phi
             }
         }
     }
@@ -35,30 +36,28 @@ public:
         color[i][j][2] = b;
     }
     inline vec3Df getColor(float & theta, float & phi){
-        // valeurs spheriques
-//        float i = (ntheta-1)*theta/180.;
-//        float j = (nphi-1)*phi/360.;
-
+        float epsilon = 0.1;
 
         float steptheta = 33.33;
         float inittheta = steptheta/2;
         float i = (theta-inittheta)/steptheta;
-        if(i<0) i=0.5;
-        if(i>ntheta) i=ntheta-1.5;
+//        if(i<0) i=0;
+//        if(i>ntheta-1) i=ntheta-1;
 
         float initphi = 0;
-        float stepphi = 36;
+        float stepphi = 34;
         float j = (phi-initphi)/stepphi;
-        if(j<0) j=0.5;
-        if(j>nphi) j=nphi-1.5;
+//        if(j<0) j=0;
+//        if(j>nphi-1) j=nphi-1;
 
-        float epsilon = 0.3;
-        if(abs(i-(int)i)<epsilon && abs(j-(int)j)<epsilon){
+        if(abs(i-(int)i)<epsilon || abs(j-(int)j)<epsilon){
+        if(i>=0 && i<ntheta-1 && j>=0 && j<nphi-1){
             vec3Df ret = color[(int)i][(int)j];
-//            ret += (i-(int)i)*(color[(int)i+1][(int)j]-color[(int)i][(int)j]);
-//            ret += (j-(int)j)*(color[(int)i][(int)j+1]-color[(int)i][(int)j]);
+            ret += (i-(int)i)*(color[(int)i+1][(int)j]-color[(int)i][(int)j]);
+            ret += (j-(int)j)*(color[(int)i][(int)j+1]-color[(int)i][(int)j]);
             return ret;
-        } else return vec3Df(0.5,0,0);
+        } else return vec3Df(1,1,1);
+        } else return vec3Df(1,1,1);
     }
 };
 
